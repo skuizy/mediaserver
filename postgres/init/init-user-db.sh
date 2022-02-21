27a30cf47b0a1d40b57897d048c9b299219a4d4b
skuizy <skuizy@users.noreply.github.com>
2022-02-22 00:22:51 +0100

    Update init-user-db.sh

--- postgres/init/init-user-db.sh
+++ postgres/init/init-user-db.sh
@@ -8,7 +8,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE "prowlarr-main";
     CREATE DATABASE "prowlarr-log";
     GRANT ALL PRIVILEGES ON DATABASE ncdb TO ncuser;
-    GRANT ALL PRIVILEGES ON DATABASE prowlarr-main TO prowlarr;
-    GRANT ALL PRIVILEGES ON DATABASE prowlarr-log TO prowlarr;
+    GRANT ALL PRIVILEGES ON DATABASE "prowlarr-main" TO prowlarr;
+    GRANT ALL PRIVILEGES ON DATABASE "prowlarr-log" TO prowlarr;
     
 EOSQL
