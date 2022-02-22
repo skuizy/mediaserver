c48ec14f9728ef8c084f8e617042eabec890a193
skuizy <skuizy@users.noreply.github.com>
2022-02-22 22:17:09 +0100

    Update init-user-db.sh

--- postgres/init/init-user-db.sh
+++ postgres/init/init-user-db.sh
@@ -2,13 +2,13 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER ncuser WITH PASSWORD 'ncpw';
-    CREATE USER prowlarr WITH PASSWORD 'prowlarr';
-    CREATE DATABASE ncdb;
-    CREATE DATABASE "prowlarr-main";
-    CREATE DATABASE "prowlarr-log";
-    GRANT ALL PRIVILEGES ON DATABASE ncdb TO ncuser;
-    GRANT ALL PRIVILEGES ON DATABASE "prowlarr-main" TO prowlarr;
-    GRANT ALL PRIVILEGES ON DATABASE "prowlarr-log" TO prowlarr;
+    CREATE USER $(cat /run/secrets/POSTGRES_NC_USER) WITH PASSWORD $(cat /run/secrets/POSTGRES_NC_PASSWORD);
+    CREATE USER $(cat /run/secrets/POSTGRES_PROWLARR_USER) WITH PASSWORD $(cat /run/secrets/POSTGRES_PROWLARR_PASSWORD);
+    CREATE DATABASE $(cat /run/secrets/POSTGRES_NC_DB);
+    CREATE DATABASE $(cat /run/secrets/POSTGRES_PROWLARR_DB_MAIN);
+    CREATE DATABASE $(cat /run/secrets/POSTGRES_PROWLARR_DB_LOG);
+    GRANT ALL PRIVILEGES ON DATABASE $(cat /run/secrets/POSTGRES_NC_DB) TO $(cat /run/secrets/POSTGRES_NC_USER);
+    GRANT ALL PRIVILEGES ON DATABASE $(cat /run/secrets/POSTGRES_PROWLARR_DB_MAIN) TO $(cat /run/secrets/POSTGRES_PROWLARR_USER);
+    GRANT ALL PRIVILEGES ON DATABASE $(cat /run/secrets/POSTGRES_PROWLARR_DB_LOG) TO $(cat /run/secrets/POSTGRES_PROWLARR_USER);
     
 EOSQL
