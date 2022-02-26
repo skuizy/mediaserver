40a13187b04030452197be0f427548bbf32a00a1
skuizy <skuizy@users.noreply.github.com>
2022-02-26 18:03:27 +0100

    Create second_run.sh

---
+++ second_run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mv ./swag/config/nginx/proxy-confs/jackett.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/jellyfin.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/lidarr.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/netdata.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/nextcloud.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/pihole.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/prowlarr.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/qbittorrent.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/radarr.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/readarr.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/sonarr.subdomain.conf{.sample,}
