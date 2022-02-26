41a6422273204ae3855a4fe6e5ea54baafc99245
skuizy <skuizy@users.noreply.github.com>
2022-02-26 18:10:38 +0100

    Update second_run.sh

--- second_run.sh
+++ second_run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-mv ./swag/config/nginx/proxy-confs/jackett.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/jellyfin.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/lidarr.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/netdata.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/nextcloud.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/pihole.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/prowlarr.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/qbittorrent.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/radarr.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/readarr.subdomain.conf{.sample,}
-mv ./swag/config/nginx/proxy-confs/sonarr.subdomain.conf{.sample,}
+mv ./swag/config/nginx/proxy-confs/jackett.subdomain.conf.sample ./swag/config/nginx/proxy-confs/jackett.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/jellyfin.subdomain.conf.sample ./swag/config/nginx/proxy-confs/jellyfin.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/lidarr.subdomain.conf.sample ./swag/config/nginx/proxy-confs/lidarr.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/netdata.subdomain.conf.sample ./swag/config/nginx/proxy-confs/netdata.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/nextcloud.subdomain.conf.sample ./swag/config/nginx/proxy-confs/nextcloud.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/pihole.subdomain.conf.sample ./swag/config/nginx/proxy-confs/pihole.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/prowlarr.subdomain.conf.sample ./swag/config/nginx/proxy-confs/prowlarr.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/qbittorrent.subdomain.conf.sample ./swag/config/nginx/proxy-confs/qbittorrent.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/radarr.subdomain.conf.sample ./swag/config/nginx/proxy-confs/radarr.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/readarr.subdomain.conf.sample ./swag/config/nginx/proxy-confs/readarr.subdomain.conf
+mv ./swag/config/nginx/proxy-confs/sonarr.subdomain.conf.sample ./swag/config/nginx/proxy-confs/sonarr.subdomain.conf
